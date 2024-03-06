5544a6a524421889e5d8a0f9420e7327db62a772
Ayman-monn <ayman10112013@gmail.com>
2024-03-06 08:32:13 +0200

    Course Template

--- courses/urls.py
+++ courses/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.courses_list, name="Courses_list"), 
+    path('course/sections/<int:cid>/', views.sections, name="Course_section"), 
+    path('course/section/lessons/<int:sid>/', views.lesson_list, name="Course_section_lessons"), 
+    path('course/section/lessons/lesson/<int:lid>/', views.lesson, name="Lesson"), 
+
+
+
 ]
--- courses/views.py
+++ courses/views.py
@@ -3,3 +3,29 @@ from .models import Course, Section, Lesson, Comment
 
 def index(request): 
     return render(request, 'index.html') 
+
+
+
+def courses_list(request):
+    courses = Course.objects.all() 
+    return render(request, 'courses/courses_list.html',{
+        'courses':courses
+    })
+
+def sections(request, cid): 
+    sections = Section.objects.select_related('course').filter(course=cid) 
+    return render(request, 'courses/sections.html', {
+        'sections':sections
+    })
+
+def lesson_list(request, sid): 
+    lessons = Lesson.objects.select_related('section').filter(section=sid)
+    return render(request, 'courses/lessons_list.html',{
+        'lessons':lessons
+    })
+
+def lesson(request, lid): 
+    lesson = Lesson.objects.get(pk=lid)
+    return render(request, 'courses/lesson.html', {
+        'lesson':lesson 
+    })
